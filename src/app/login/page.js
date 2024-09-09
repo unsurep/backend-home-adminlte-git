91a580861d36cdc01c8ee176f6938a7835959893
Oghenevwede Louis Umukoro <wedevilleg@gmail.com>
2024-09-09 16:23:17 +0100

    updated json path

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -4,7 +4,7 @@
 import Link from "next/link"
 import Image from "next/image"
 import { useState, useEffect } from "react"
-import Login from "@/components/Login/Login";
+import Login from "@/components/Login/login";
 
 
 
